26134acb45da747c13ecc5c12ade8cb2af7faceb
Lidsy <hagta.lidsy@escolar.ifrn.edu.br>
2026-09-12 19:57:02 -0300

    Adiciona atividade 4

--- src/main/java/ifrn/pi/eventos/controllers/EventoController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventoController.java
@@ -1,15 +1,68 @@
 package ifrn.pi.eventos.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ifrn.pi.eventos.models.Eventos;
+import ifrn.pi.eventos.repositories.EventosRepository;
 
 @Controller
+@RequestMapping("/eventos")
 public class EventoController {
 
-	@RequestMapping("/eventos/form")
-	public String form() {
-		return "formEvento" ; 
-	}
-	
-	
+    @Autowired
+    private EventosRepository er;
+
+    @GetMapping("/form")
+    public String form() {
+        return "eventos/formEvento";
+    }
+
+    @PostMapping
+    public String adicionar(Eventos evento) {
+
+        System.out.println(evento);
+        er.save(evento);
+
+        return "redirect:/eventos";
+    }
+
+    @GetMapping
+    public ModelAndView listar() {
+
+        List<Eventos> eventos = er.findAll();
+
+        ModelAndView mv = new ModelAndView("eventos/lista");
+        mv.addObject("eventos", eventos);
+
+        return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) {
+
+        ModelAndView md = new ModelAndView();
+
+        Optional<Eventos> opt = er.findById(id);
+
+        if (opt.isEmpty()) {
+            md.setViewName("redirect:/eventos");
+            return md;
+        }
+
+        Eventos evento = opt.get();
+
+        md.setViewName("eventos/detalhes");
+        md.addObject("evento", evento);
+
+        return md;
+    }
 }
--- src/main/java/ifrn/pi/eventos/models/Eventos.java
+++ src/main/java/ifrn/pi/eventos/models/Eventos.java
@@ -1,43 +1,65 @@
 package ifrn.pi.eventos.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Eventos {
 
-	private String nome;
-	private String local;
-	private String data;
-	private String horario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public String getNome() {
-		return nome;
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
 
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getLocal() {
-		return local;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setLocal(String local) {
-		this.local = local;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getData() {
-		return data;
-	}
+    public String getLocal() {
+        return local;
+    }
 
-	public void setData(String data) {
-		this.data = data;
-	}
+    public void setLocal(String local) {
+        this.local = local;
+    }
 
-	public String getHorario() {
-		return horario;
-	}
+    public String getData() {
+        return data;
+    }
 
-	public void setHorario(String horario) {
-		this.horario = horario;
-	}
+    public void setData(String data) {
+        this.data = data;
+    }
 
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return "Eventos [id=" + id + ", nome=" + nome + ", local=" + local
+                + ", data=" + data + ", horario=" + horario + "]";
+    }
 }
